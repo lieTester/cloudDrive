7b7c7e984bf7b6e3e086b173bec9f638961e1121
puneet khandal <lietester.00@gmail.com>
2023-09-16 11:10:06 +0530

    added session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { AuthProvider } from "@/hook/_appAuth";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -17,7 +18,7 @@ export default function RootLayout({
    return (
       <html lang="en">
          <body className={inter.className}>
-            {children}
+            <AuthProvider>{children}</AuthProvider>
          </body>
       </html>
    );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 "use client";
+import { useSession } from "next-auth/react";
+import { useState, useEffect } from "react"
 
 export default function Home() {
+   const { data: session } = useSession();
+   const [isLoding, setIsLoding] = useState(true);
+   useEffect(() => {
+      console.log(isLoding);
+      if (session) setIsLoding(false);
+      setTimeout(() => {
+         setIsLoding(false);
+      }, 200);
+   }, [session]);
+
    return (
       <main className="w-screen h-screen bg-prim1 font-openSans">
-         clone-drive
+         {!session ? (
+            <div className=""> in</div>
+         ) : (
+            <div>out</div>
+         )}
       </main>
    );
 }
