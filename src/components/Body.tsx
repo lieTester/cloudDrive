0f730831d2b6eb458b864a81313ae355313af027
puneet khandal <lietester.00@gmail.com>
2023-09-18 01:10:06 +0530

    head and body comp

---
+++ src/components/Body.tsx
@@ -0,0 +1,5 @@
+const Body = () => {
+   return <div className="h-[92%] p-2 flex ">body</div>;
+};
+
+export default Body;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+   return (
+      <div className="w-full h-[8%] flex p-2 !important  text-prim1 ">
+         Header
+      </div>
+   );
+};
+
+export default Header;
