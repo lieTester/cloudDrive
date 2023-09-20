0e73c6ecddb1eb3476d0111c7572cf868f8cf04f
puneet khandal <lietester.00@gmail.com>
2023-09-20 09:10:34 +0530

    main component ui

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,7 +3,20 @@ import { BsCardList } from "react-icons/bs";
 const Main = () => {
    return (
       <>
-         body
+         <div className="h-full w-[79%] bg-prim2 rounded-xl p-3">
+            <ul className="inline-block [&>li]:text-2xl [&>li]:px-4 [&>li]:py-1 [&>li]:rounded-full">
+               <li className="hover:bg-seco2">My Drive</li>
+            </ul>
+            <ul className="float-right mr-3 flex [&>li]:m-1">
+               <li className="flex p-2  hover:bg-seco2 align-middle rounded-full">
+                  <BsCardList className="text-xl " />
+               </li>
+               <li className="flex p-2  hover:bg-seco2 align-middle rounded-full">
+                  <BiInfoCircle className="text-xl " />
+               </li>
+            </ul>
+         </div>
+         <div className="w-auto"></div>
       </>
    );
 };
