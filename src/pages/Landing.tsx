e7269a803e464b83d725ad462e75142cb1097ed0
puneet khandal <lietester.00@gmail.com>
2023-10-05 14:05:11 +0530

    landing page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import Body from "@/components/Body";
+import Header from "@/components/Header";
+
+const Landing = () => {
+   return (
+      <>
+         <Header />
+         <Body />
+      </>
+   );
+};
+
+export default Landing;
