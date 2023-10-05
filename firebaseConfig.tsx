8812cac69037c9ddce370651f5c98d0b10ec3c63
puneet khandal <lietester.00@gmail.com>
2023-10-05 14:26:15 +0530

    changes

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-   apiKey: process.env.FIREBASE_apiKey,
-   authDomain: process.env.FIREBASE_authDomain,
-   projectId: process.env.FIREBASE_projectId,
-   storageBucket: process.env.FIREBASE_storageBucket,
-   messagingSenderId: process.env.FIREBASE_messagingSenderId,
-   appId: process.env.FIREBASE_appId,
-   measurementId: process.env.FIREBASE_measurementId,
+   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
