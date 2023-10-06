bc9da43adf7a2a3c1bafffa56f581638680c420e
puneet khandal <lietester.00@gmail.com>
2023-10-06 12:41:41 +0530

    removed isLoading direct from session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
 import Login from "./Auth/page";
 import Landing from "@/pages/Landing";
 import LoadingPage from "@/components/subcomponent/LoderComponent";
@@ -10,21 +9,12 @@ import { FileFolderProvider } from "../context/FileDataContext";
 export default function Home() {
    // session
    const { data: session } = useSession();
-   const [isLoding, setIsLoding] = useState(true);
-   useEffect(() => {
-      console.log(isLoding);
-      if (session) {
-         setIsLoding(false);
-      }
-      setTimeout(() => {
-         setIsLoding(false);
-      }, 300);
-   }, [session]);
+   console.log(session);
 
    return (
       <main className="w-screen h-screen bg-prim1 ">
-         {!isLoding ? (
-            session ? (
+         {session !== undefined ? (
+            session?.user ? (
                <FileFolderProvider>
                   <Landing session={session} />
                </FileFolderProvider>
